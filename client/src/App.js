1f16748be605f7d404e1279b2ae1ecf3d8dcaea8
[firstname <yaaseenkhan02@gmail.com>
2023-02-23 15:44:21 -0500

    front end dashboard

---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Blank from './pages/Blank'
+import Dashboard from './pages/Dashboard'
+import MainLayout from './layout/MainLayout'
+
+import './App.css';
+
+
+import GlobalStyles from "./GlobalStyles";
+
+
+// import { Navigation } from "./components/Navigation/Navigation";
+
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
+function App() {
+
+  return (
+
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<MainLayout />}>
+                    <Route index element={<Dashboard />} />
+                    <Route path="newTounament" element={<Blank />} />
+                    <Route path="products" element={<Blank />} />
+                    <Route path="customers" element={<Blank />} />
+                    <Route path="settings" element={<Blank />} />
+                    <Route path="stats" element={<Blank />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    </ApolloProvider>
+  );
+}
+
+export default App;
